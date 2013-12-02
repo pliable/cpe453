6cce011b176170389dfa8e82f160203bbf5bf841
kestein <kestein@calpoly.edu>
2013-12-02 12:26:00 -0800

    filled some shit in, spooky test driver

--- prog4/libDisk.c
+++ prog4/libDisk.c
@@ -1,22 +1,46 @@
 #include "fs.h"
 
 int openDisk(char *filename, int nBytes) {
-   int disk;
+   int disk, c;
    int numBlocks = nBytes/BLOCKSIZE;
    disk = open(filename, O_RDWR);/* Put numBytes in entry table for this disk */
-   
+   if(disk < 0) {
+      return -1;
+   }
+   for(c = 0; c < nBytes; c++) {
+      write(disk, 0, 1);
+   }
+
+   return disk;
 }
 
 int readBlock(int disk, int bNum, void *block) {
-   int offset = bNum*BLOCKSIZE;
+   int offset = bNum*BLOCKSIZE, status;
    void buffer[BLOCKSIZE];
    /* cHECK to make sure offset < allocated disk bytes */
-   lseek(disk, offset, SEEK_SET);
-   read(disk, buffer, BLOCKSIZE);
+   status = lseek(disk, offset, SEEK_SET);
+   if(status < 0) {
+      return -1;
+   }
+   status = read(disk, buffer, BLOCKSIZE);
+   if(status < 0) {
+      return -1;
+   }
 
    return 0;
 }
 
 int writeBlock(int disk, int bNum, void *block) {
-   
+   int offset = bNum*BLOCKSIZE, status;
+   /* cHECK to make sure offset < allocated disk bytes */
+   status = lseek(disk, offset, SEEK_SET);
+   if(status < 0) {
+      return -1;
+   }
+   status = write(disk, block, BLOCKSIZE);
+   if(status < 0) {
+      return -1;
+   }
+
+   return 0;
 }
---
+++ prog4/tester.c
@@ -0,0 +1,5 @@
+#include "libDisk.c"
+
+int main() {
+   
+}
