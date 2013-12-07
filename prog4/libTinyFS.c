004bd9bc0c4389cce53581b6651d2ae4bf3080f3
Steve Choo <pliable@gmail.com>
2013-12-06 16:06:56 -0800

    Implemented mkfs

--- prog4/libTinyFS.c
+++ prog4/libTinyFS.c
@@ -4,7 +4,53 @@ fileDescriptor globalFP = 0;
 struct node head;
 
 int tfs_mkfs(char *filename, int nBytes) {
+   int disk, c, offset;
+   int numBlocks = nBytes/BLOCKSIZE;
+   int lengthOfBitVector = numBlocks / 8;
+   superblock sb;
+   formatted_block fb;
 
+   disk = openDisk(filename, nBytes);
+
+   for(c = 0; c < nBytes; c++) {
+      SYS_ERR(write(disk, "\0", 1), "write");
+   }
+
+   SYS_ERR(lseek(disk, 0, SEEK_SET), "lseek");//error check yo
+   //superblock setup
+
+   sb.type = 1;
+   sb.magic = MAGIC;
+   sb.byteOffset = sizeof(superblock) + lengthOfBitVector;
+   sb.finalByte = 0;
+
+   //Writing superblock to disk
+   SYS_ERR(write(disk, &sb, sizeof(sb)), "write");
+   
+   //format the blocks on the disk
+
+   fb.type = 4;
+   fb.magic = MAGIC;
+   fb.finalByte = 0;
+
+   for(c = 1; c < numBlocks; c++) {//start at block 1 cos block 0 is the super
+      SYS_ERR(lseek(disk, BLOCKSIZE*c, SEEK_SET), "lseek");
+
+      if(c + 1 > numBlocks) {//we are on the final block, so there is no next
+         fb.blockAddress = "\0";
+      }
+      else {
+         fb.blockAddress = c + 1;
+         offset = c*BLOCKSIZE;
+      }
+      //This will always execute in the loop...what about final block write?
+      SYS_ERR(write(disk, &fb, sizeof(fb)), "write");
+   }
+
+   /* Every system call will exit out and return another number,
+    * if we reach here, then everything should be successful
+    */
+   return 0;
 }
 
 int tfs_mount(char *filename) {
