963a61da10623ee00a1dc2ff4b5a5e05bad5ffd9
Steve Choo <pliable@gmail.com>
2013-12-06 17:32:19 -0800

    Added lseek to make file certain size

--- prog4/libDisk.c
+++ prog4/libDisk.c
@@ -7,20 +7,20 @@ int openDisk(char *filename, int nBytes) {
    int disk, numBytes = (nBytes/BLOCKSIZE) * BLOCKSIZE;//to make sure bytes is a multiple of the blocksize
    struct stat checker;
 
-   if(lstat(filename, &stat) < 0) {/* Create file if it does not exist */
-      SYS_ERR((disk = (open(filename, O_CREAT))), "open");
-      SYS_ERR(((write(disk, nBytes, sizeof(int)))), "write");/* write the size of the "HDD" at the beginning */
+   if(lstat(filename, &checker) < 0) {/* Create file if it does not exist */
+      SYS_ERR((disk = (open(filename, O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH))), "open");
    }
    else {
       if(nBytes == 0) { /* If the contents should not be overwritten */
          SYS_ERR((disk = (open(filename, O_RDWR))), "open");
-         SYS_ERR(((lseek(disk, 0, SEEK_END))), "lseek");
+         SYS_ERR(lseek(disk, 0, SEEK_END), "lseek");
       }
       else { /* If contents can be overwritten */
          SYS_ERR((disk = (open(filename, O_RDWR))), "open");
-         SYS_ERR(((write(disk, nBytes, sizeof(int)))), "write");/* write the size of the "HDD" at the beginning */
       }
    }
+
+   SYS_ERR(lseek(disk, nBytes, SEEK_SET), "lseek");
 
    return disk;
 }
